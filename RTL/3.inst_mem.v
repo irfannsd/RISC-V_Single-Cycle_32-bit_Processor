7d69141493e9b52862479dc0e6b182ee6be309dd
IRFAN KHAN <irfan.nsd01@gmail.com>
2026-04-05 06:24:23 +0000

    fix

--- RTL/3.inst_mem.v
+++ RTL/3.inst_mem.v
@@ -5,10 +5,15 @@ module Inst_Mem(
 
 reg [31:0] mem [0:1023];
 
-assign instruction = mem[PC[31:2]];
+// assign instruction = mem[PC[31:2]];
+assign instruction =(PC[31:2] < 1024) ? mem[PC[31:2]] : 32'h00000013;
+
+integer i;
 
 initial begin
-    $readmemh("instr_mem.hex", mem);
-end
+    for(i=0;i<1024;i=i+1)
+        mem[i] = 32'b0;
 
+    $readmemh("4.instr_mem.hex", mem);
+end
 endmodule
